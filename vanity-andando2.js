58ac2a542e850f42eee5d0988145f05c4ddf0e5e
reyhaker <reyhaker@adinet.com.uy>
2019-07-13 15:14:45 -0300

    Add files via upload

---
+++ vanity-andando2.js
@@ -0,0 +1,21 @@
+var bitcoin = require("bitcoinjs-lib");
+var hit = 0;
+var tryN = 0;
+var CoinKey = require('coinkey') //1.0.0
+
+while(hit < 1)
+{
+var ck = new CoinKey.createRandom()
+
+var pkey = ck.privateWif;
+
+var address = ck.publicAddress
+var vanity = address.substring(0,6);
+console.log(tryN + " " + vanity);
+if (vanity == "1BaFZr" || vanity == "1FeexV")     {
+	console.log(address + " , " + pkey);
+	hit = 2;
+
+}
+	tryN++;
+}
